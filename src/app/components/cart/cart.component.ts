bc564def6c8cb98b909145925954ea3ef0091230
Zahira <zrivrio2103@gmail.com>
2025-02-18 19:19:50 +0100

    mostrar pecio en carrito

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder } from '@angular/forms';
 
 import { CartService } from '../../service/cart.service';
 import { Product } from '../../models/products';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-cart',
@@ -11,11 +12,17 @@ import { Product } from '../../models/products';
     standalone: false
 })
 export class CartComponent {
+  items: Product[] = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+    shipping: '' // Add shipping to the form group
+  });
+  shippingCosts!: Observable<{ type: string, price: number }[]>;
 
-  items = this.cartService.getItems();
-  checkoutForm = this.formBuilder.group({ name: '', address: '' });
-  
-  constructor(private cartService: CartService, private formBuilder: FormBuilder) { }
+  constructor(private cartService: CartService, private formBuilder: FormBuilder) {
+    this.shippingCosts = this.cartService.getShippingPrices();
+  }
 
   onSubmit(): void {
     // Process checkout data here
@@ -26,5 +33,7 @@ export class CartComponent {
 
   removeItem(product: Product) {
     this.cartService.removeFromCart(product);
+    // Update the items array after removal
+    this.items = this.cartService.getItems();
   }
 }
