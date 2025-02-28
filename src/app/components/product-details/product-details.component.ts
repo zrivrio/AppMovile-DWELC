24435dc598e5faa1b907a152f2f3736332d5b7a4
Zahira <zrivrio2103@gmail.com>
2025-02-28 16:01:02 +0100

    mostrar los detalles de los productos del json

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,31 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
 import { Product, products } from '../../models/products';
 import { CartService } from '../../service/cart.service';
 
 @Component({
-    selector: 'app-product-details',
-    templateUrl: './product-details.component.html',
-    styleUrl: './product-details.component.css',
-    standalone: false
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'], // 🔹 Corrección en styleUrls
+  standalone: false
 })
 export class ProductDetailsComponent implements OnInit {
-  
+  products: Product[] = [...products]; // Productos locales
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute, private cartService: CartService) { }
+  constructor(private route: ActivatedRoute, private cartService: CartService) {}
 
   ngOnInit() {
-    // First get the product id from the current route.
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
     if(!routeParams){
     alert("No se han encontrado datos")
     }
-  
     // Find the product that correspond with the id provided in route.
-    this.product = products.find(product => product.id === productIdFromRoute);
+    this.product = this.products.find(product => product.id === productIdFromRoute);
+
+    this.cartService.getItemJson().subscribe(products2 => {
+      const productsMap = products2.map(p => ({
+        id: p.product_id,
+        name: p.product_name,
+        price: p.cost,
+        description: p.detail,
+        provider: { id: Number(p.suppiler), name: "" }
+      }));
+
+      this.products = [...this.products, ...productsMap];
+
+      if (!this.product) {
+        this.product = this.products.find(product => product.id === productIdFromRoute);
+      }
+    });
   }
 
   addToCart(product: Product) {
